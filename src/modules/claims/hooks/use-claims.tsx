a331577acbb69e4e569927e396331d11105bcce2
JoaquinAdan <rodriguezjdev@gmail.com>
2024-09-10 11:13:05 -0300

    feat: Add lazy loading for users and electronic address views
    Add lazy loading for users and electronic address views to improve performance and reduce initial load time.
    
    - Update .gitignore to ignore .env files.
    - Add new file src/configs/constants/api-base.ts to define the API base URL.
    - Add new file src/modules/claims/hooks/use-claims.tsx to handle claims data fetching.
    - Add new file src/modules/claims/services/get-claims.tsx to make HTTP request for claims data.

---
+++ src/modules/claims/hooks/use-claims.tsx
@@ -0,0 +1,16 @@
+import { useQuery } from '@tanstack/react-query'
+import { getClaimsHttp } from '../services/get-claims'
+import { FilterDto } from '@/modules/shared/models/filter'
+
+export const GET_CLAIMS = 'GET_CLAIMS'
+
+export const useClaims = (filters: FilterDto) => {
+  const token = ''
+  const pokemon = useQuery<any>({
+    queryKey: [GET_CLAIMS, filters],
+    queryFn: () => getClaimsHttp(token, filters),
+    refetchOnWindowFocus: false,
+  })
+
+  return pokemon
+}
