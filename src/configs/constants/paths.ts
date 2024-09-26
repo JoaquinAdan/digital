dbf02b23e783115759aba1907b5490e917ef01c2
JoaquinAdan <rodriguezjdev@gmail.com>
2024-09-26 12:49:03 -0300

    Refactor admin paths in constants/paths.ts

--- src/configs/constants/paths.ts
+++ src/configs/constants/paths.ts
@@ -1,12 +1,4 @@
 const PATHS = {
-<<<<<<< HEAD
-  HOME: '/',
-  CONTACTS: '/contacts',
-  ADMIN_CLAIMS: '/admin/reclamos',
-  EXAMPLE: '/ejemplo',
-  SOCIAL_ADMIN: '/admin/social-services',
-  SOCIAL_PUBLIC: '/social-services'
-=======
   PUBLICS: {
     ELECTRONIC_ADDRESS: '/domicilio-electronico',
     CONTACTS: '/contacts',
@@ -19,7 +11,6 @@ const PATHS = {
   NEIGHBOR: {},
   ADMIN: { CLAIMS: '/reclamos', CLAIMS_DETAIL: '/reclamos/detail' },
   DIGITAL: { USERS: '/usuarios' },
->>>>>>> e406cae96af548c24d8354cdce68d460a4952336
 }
 
 export default PATHS
